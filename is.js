9e4d4f35cdb7a1e088c5538c44d3be888b4d7463
tvairakt <t.vairaktaris@gmail.com>
2026-05-13 09:07:34 +0300

    add is

---
+++ is.js
@@ -0,0 +1,11 @@
+is.num = (v) => typeof v === "number" && !Number.isNaN(v);
+is.nan = (v) => Number.isNaN(v);
+is.str = (v) => typeof v === "string";
+is.bool = (v) => typeof v === "boolean";
+is.undef = (v) => v === undefined;
+is.def = (v) => v !== undefined;
+is.arr = (v) => Array.isArray(v);
+is.obj = (v) => typeof v === "object" && !Array.isArray(v);
+is.fun = (v) => typeof v === "function";
+is.truthy = (v) => !!v;
+is.falsy = (v) => !v;
