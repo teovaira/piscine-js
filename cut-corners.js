6e4e416ce127ffd4cbdd5f23dccbb27afbe9d590
tvairakt <t.vairaktaris@gmail.com>
2026-05-16 08:56:31 +0300

    fix round for negative numbers using absolute fractional part

--- cut-corners.js
+++ cut-corners.js
@@ -12,4 +12,8 @@ const floor = (n) => (trunc(n) === n ? n : n < 0 ? trunc(n) - 1 : trunc(n));
 
 const ceil = (n) => (trunc(n) === n ? n : n < 0 ? trunc(n) : trunc(n) + 1);
 
-const round = (n) => (n - trunc(n) >= 0.5 ? ceil(n) : floor(n));
+const round = (n) => {
+  const frac = n - trunc(n);
+  const absFrac = frac < 0 ? -frac : frac;
+  return absFrac >= 0.5 ? ceil(n) : floor(n);
+};
