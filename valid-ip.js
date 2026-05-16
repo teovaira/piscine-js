6d9bac001c7c89654bb440448971414761471c52
tvairakt <t.vairaktaris@gmail.com>
2026-05-16 12:16:53 +0300

    fix valid-ip to not match bare IP when followed by a port colon

--- valid-ip.js
+++ valid-ip.js
@@ -3,7 +3,7 @@ const findIP = (str) => {
   const ip = `${octet}\\.${octet}\\.${octet}\\.${octet}`;
   const port =
     "(:(6553[0-5]|655[0-2]\\d|65[0-4]\\d\\d|6[0-4]\\d{3}|[1-5]\\d{4}|[1-9]\\d{1,3}|\\d))?";
-  const pattern = new RegExp(`(?<![\\d.])${ip}${port}(?![\\d.])`, "g");
+  const pattern = new RegExp(`(?<![\\d.])${ip}${port}(?![\\d.:])`, "g");
   const matches = [...str.matchAll(pattern)];
   return matches.map((m) => m[0]);
 };
