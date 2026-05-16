acea581b8a6ec9775f9b34ab43ff77416bccb749
tvairakt <t.vairaktaris@gmail.com>
2026-05-16 12:14:47 +0300

    fix group-price to match currency symbols like $ in addition to 3-letter codes

--- group-price.js
+++ group-price.js
@@ -1,4 +1,4 @@
 const groupPrice = (str) => {
-  const matches = [...str.matchAll(/[A-Z]{3}(\d+)\.(\d+)/g)];
-  return matches.map((m) => [m[0], m[1], m[2]]);
+  const matches = [...str.matchAll(/([A-Z]{3}|[\$€£])(\d+)\.(\d+)/g)];
+  return matches.map((m) => [m[0], m[2], m[3]]);
 };
