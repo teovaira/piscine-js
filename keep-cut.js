fe65f2edde529977d4d39ffdef6b7e306ffa893a
tvairakt <t.vairaktaris@gmail.com>
2026-05-16 08:43:29 +0300

    fix keepFirstLast for strings of length 2 or less

--- keep-cut.js
+++ keep-cut.js
@@ -1,6 +1,7 @@
 const cutFirst = (str) => str.slice(2);
 const cutLast = (str) => str.slice(0, str.length - 2);
 const cutFirstLast = (str) => str.slice(2, str.length - 2);
-const keepFirst = (str) => str.slice(0, 2);
+const keepFirstLast = (str) =>
+  str.length <= 2 ? str : str.slice(0, 2) + str.slice(str.length - 2);
 const keepLast = (str) => str.slice(str.length - 2);
 const keepFirstLast = (str) => str.slice(0, 2) + str.slice(str.length - 2);
