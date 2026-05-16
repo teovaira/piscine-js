9b5b93aa4ef7b72387b8e7cd4691189b3846be8e
tvairakt <t.vairaktaris@gmail.com>
2026-05-16 09:23:43 +0300

    add triangle

---
+++ triangle.js
@@ -0,0 +1,8 @@
+const triangle = (char, height) => {
+  let result = "";
+  for (let i = 1; i <= height; i++) {
+    result += char.repeat(i);
+    if (i < height) result += "\n";
+  }
+  return result;
+};
