da1aaff73d0ca7fa69c91d2a540910e1d079555d
tvairakt <t.vairaktaris@gmail.com>
2026-05-16 08:48:13 +0300

    fix keepFirstLast for strings of length 3

--- keep-cut.js
+++ keep-cut.js
@@ -1,7 +1,6 @@
-const cutFirst = (str) => str.slice(2);
-const cutLast = (str) => str.slice(0, str.length - 2);
-const cutFirstLast = (str) => str.slice(2, str.length - 2);
-const keepFirst = (str) => str.slice(0, 2);
-const keepLast = (str) => str.slice(str.length - 2);
-const keepFirstLast = (str) =>
-  str.length <= 2 ? str : str.slice(0, 2) + str.slice(str.length - 2);
+const cutFirst = (str) => str.slice(2)
+const cutLast = (str) => str.slice(0, str.length - 2)
+const cutFirstLast = (str) => str.slice(2, str.length - 2)
+const keepFirst = (str) => str.slice(0, 2)
+const keepLast = (str) => str.slice(str.length - 2)
+const keepFirstLast = (str) => str.length <= 3 ? str : str.slice(0, 2) + str.slice(str.length - 2)
