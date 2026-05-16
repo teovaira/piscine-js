6196925808b892c07445bd9d973ae3cde22918c9
tvairakt <t.vairaktaris@gmail.com>
2026-05-16 09:13:34 +0300

    add reverser

---
+++ reverser.js
@@ -0,0 +1,8 @@
+const reverse = (input) => {
+  const result = typeof input === "string" ? "" : [];
+  for (let i = input.length - 1; i >= 0; i--) {
+    if (typeof input === "string") result += input[i];
+    else result.push(input[i]);
+  }
+  return result;
+};
