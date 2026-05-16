792c0da00bc7b6db049af720bea7d802c002bc9b
tvairakt <t.vairaktaris@gmail.com>
2026-05-16 09:14:49 +0300

    fix reverse: use let for result to allow string concatenation

--- reverser.js
+++ reverser.js
@@ -1,5 +1,5 @@
 const reverse = (input) => {
-  const result = typeof input === "string" ? "" : [];
+  let result = typeof input === "string" ? "" : [];
   for (let i = input.length - 1; i >= 0; i--) {
     if (typeof input === "string") result += input[i];
     else result.push(input[i]);
