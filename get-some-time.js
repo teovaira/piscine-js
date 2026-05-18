e66ec3a4efd7c4b57512b4b9cc14a28b86c64536
tvairakt <t.vairaktaris@gmail.com>
2026-05-18 19:47:56 +0300

    add get-some-time

---
+++ get-some-time.js
@@ -0,0 +1,17 @@
+const firstDayWeek = (week, year) => {
+  const jan1 = new Date(`${year}-01-01`);
+  const jan1Day = jan1.getDay();
+  const mondayOffset = jan1Day === 0 ? -6 : 1 - jan1Day;
+  const firstMonday = new Date(jan1);
+  firstMonday.setDate(1 + mondayOffset);
+
+  const target = new Date(firstMonday);
+  target.setDate(firstMonday.getDate() + (week - 1) * 7);
+
+  if (target.getFullYear() < Number(year)) return `01-01-${year}`;
+
+  const dd = String(target.getDate()).padStart(2, "0");
+  const mm = String(target.getMonth() + 1).padStart(2, "0");
+  const yyyy = target.getFullYear();
+  return `${dd}-${mm}-${yyyy}`;
+};
