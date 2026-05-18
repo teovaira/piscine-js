981d8baebd71a8ef52d6a8d3dae7c1240b8314f4
tvairakt <t.vairaktaris@gmail.com>
2026-05-18 20:17:49 +0300

    fix day-of-the-year for years 0-99 using setFullYear

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,5 +1,7 @@
 const dayOfTheYear = (date) => {
-  const start = new Date(date.getFullYear(), 0, 1);
+  const start = new Date(0);
+  start.setFullYear(date.getFullYear(), 0, 1);
+  start.setHours(0, 0, 0, 0);
   const msPerDay = 24 * 60 * 60 * 1000;
   return Math.round((date - start) / msPerDay) + 1;
 };
