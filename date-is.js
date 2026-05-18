dcb440424aac70b218248fecc96709fdfd9188a8
tvairakt <t.vairaktaris@gmail.com>
2026-05-18 19:43:38 +0300

    add date-is

---
+++ date-is.js
@@ -0,0 +1,9 @@
+const isValid = (date) => date instanceof Date && !isNaN(date.getTime());
+
+const isAfter = (date1, date2) => date1 > date2;
+
+const isBefore = (date1, date2) => date1 < date2;
+
+const isFuture = (date) => isValid(date) && date > new Date();
+
+const isPast = (date) => isValid(date) && date < new Date();
