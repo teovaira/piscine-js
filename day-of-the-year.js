43f1dc60b766bd3b367697b9752bbd4def8fa278
tvairakt <t.vairaktaris@gmail.com>
2026-05-18 20:14:56 +0300

    fix day-of-the-year to count from 1 not 0

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,5 +1,5 @@
 const dayOfTheYear = (date) => {
   const start = new Date(date.getFullYear(), 0, 1);
   const msPerDay = 24 * 60 * 60 * 1000;
-  return Math.round((date - start) / msPerDay);
+  return Math.round((date - start) / msPerDay) + 1;
 };
