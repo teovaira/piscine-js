2e69a512fbb94835c9359ee4a9f3e54725187e16
tvairakt <t.vairaktaris@gmail.com>
2026-05-19 19:43:33 +0300

    add long-words

---
+++ long-words.js
@@ -0,0 +1,6 @@
+const longWords = (arr) => arr.every((str) => str.length >= 5);
+
+const oneLongWord = (arr) => arr.some((str) => str.length >= 10);
+
+const noLongWords = (arr) => !arr.some((str) => str.length >= 7);
+
