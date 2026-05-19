353e72664f0afa1f8fd27e982dde6c851c02f5b6
tvairakt <t.vairaktaris@gmail.com>
2026-05-19 19:24:56 +0300

    add using-map

---
+++ using-map.js
@@ -0,0 +1,30 @@
+const citiesOnly = (arr) => arr.map((obj) => obj.city);
+
+const upperCasingStates = (arr) =>
+  arr.map((str) =>
+    str
+      .split(" ")
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(" "),
+  );
+
+const fahrenheitToCelsius = (arr) =>
+  arr.map((str) => {
+    const f = parseInt(str);
+    return Math.floor(((f - 32) * 5) / 9) + "°C";
+  });
+
+const trimTemp = (arr) =>
+  arr.map((obj) => ({
+    city: obj.city,
+    temperature: obj.temperature.replace(/\s/g, ""),
+  }));
+
+const tempForecasts = (arr) =>
+  arr.map((obj) => {
+    const f = parseInt(obj.temperature);
+    const c = Math.floor(((f - 32) * 5) / 9);
+    const city = obj.city;
+    const state = obj.state[0].toUpperCase() + obj.state.slice(1);
+    return `${c}°Celsius in ${city}, ${state}`;
+  });
