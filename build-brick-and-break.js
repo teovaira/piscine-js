697f05dddf073d700f0f61b46b5c9a8a60f3701d
tvairakt <t.vairaktaris@gmail.com>
2026-05-22 09:51:36 +0300

    add build-brick-and-break

---
+++ build-brick-and-break.js
@@ -0,0 +1,35 @@
+let brickCount = 0;
+
+const build = (amount) => {
+  let built = 0;
+  const interval = setInterval(() => {
+    built++;
+    brickCount++;
+    const brick = document.createElement("div");
+    brick.id = `brick-${brickCount}`;
+    const column = brickCount % 3;
+    if (column === 2) brick.dataset.foundation = true;
+    document.body.append(brick);
+    if (built >= amount) clearInterval(interval);
+  }, 100);
+};
+
+const repair = (...ids) => {
+  for (const id of ids) {
+    const brick = document.getElementById(id);
+    if (!brick) continue;
+    if (brick.hasAttribute("data-foundation")) {
+      brick.dataset.repaired = "in progress";
+    } else {
+      brick.dataset.repaired = "true";
+    }
+  }
+};
+
+const destroy = () => {
+  const brick = document.getElementById(`brick-${brickCount}`);
+  if (brick) {
+    brick.remove();
+    brickCount--;
+  }
+};
