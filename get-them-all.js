d6448c7dad8520e130c40b616371f8039d9fee9e
tvairakt <t.vairaktaris@gmail.com>
2026-05-22 09:45:10 +0300

    add get-them-all

---
+++ get-them-all.js
@@ -0,0 +1,27 @@
+export const getArchitects = () => {
+  const architects = [...document.getElementsByTagName("a")];
+  const nonArchitects = [...document.querySelectorAll(":not(a)")];
+  return [architects, nonArchitects];
+};
+
+export const getClassical = () => {
+  const classical = [...document.querySelectorAll("a.classical")];
+  const nonClassical = [...document.querySelectorAll("a:not(.classical)")];
+  return [classical, nonClassical];
+};
+
+export const getActive = () => {
+  const active = [...document.querySelectorAll("a.classical:not(.active)")];
+  const nonActive = [...document.querySelectorAll("a.classical.active")];
+  return [active, nonActive];
+};
+
+export const getBonannoPisano = () => {
+  const bonanno = document.getElementById("BonannoPisano");
+  const rest = [
+    ...document.querySelectorAll(
+      "a.classical:not(.active):not(#BonannoPisano)",
+    ),
+  ];
+  return [bonanno, rest];
+};
