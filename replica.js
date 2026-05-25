ef33a9615b221509d129d3bdb01527874468eb01
tvairakt <t.vairaktaris@gmail.com>
2026-05-25 23:13:44 +0300

    add replica

---
+++ replica.js
@@ -0,0 +1,21 @@
+const replica = (target, ...sources) => {
+  for (const source of sources) {
+    for (const key in source) {
+      if (!source.hasOwnProperty(key)) continue;
+      if (
+        source[key] !== null &&
+        typeof source[key] === "object" &&
+        !Array.isArray(source[key]) &&
+        !(source[key] instanceof RegExp)
+      ) {
+        if (target[key] === null || typeof target[key] !== "object") {
+          target[key] = {};
+        }
+        replica(target[key], source[key]);
+      } else {
+        target[key] = source[key];
+      }
+    }
+  }
+  return target;
+};
