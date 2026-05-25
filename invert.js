7eeee237cdd6d84f90ad0b33f17caee29621aa54
tvairakt <t.vairaktaris@gmail.com>
2026-05-25 22:03:25 +0300

    fix invert to ignore prototype chain properties

--- invert.js
+++ invert.js
@@ -1,7 +1,7 @@
 const invert = (obj) => {
   const result = {};
   for (const key in obj) {
-    result[obj[key]] = key;
+    if (obj.hasOwnProperty(key)) result[obj[key]] = key;
   }
   return result;
 };
