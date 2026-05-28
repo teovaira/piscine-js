71f543908bb571e87ed1c2724bea635db5c3c691
tvairakt <t.vairaktaris@gmail.com>
2026-05-28 10:06:54 +0300

    fix timeout: clear timer when callback resolves first

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,23 +1,16 @@
-const retry =
-  (count, callback) =>
-  async (...args) => {
-    let attempts = 0;
-    while (true) {
-      try {
-        return await callback(...args);
-      } catch (err) {
-        attempts++;
-        if (attempts > count)
-          throw new Error(err.message || "max retries reached");
-      }
-    }
-  };
-
 const timeout =
   (delay, callback) =>
   async (...args) => {
-    const timer = new Promise((_, reject) =>
-      setTimeout(() => reject(new Error("timeout")), delay),
-    );
-    return Promise.race([callback(...args), timer]);
+    let timerId;
+    const timer = new Promise((_, reject) => {
+      timerId = setTimeout(() => reject(new Error("timeout")), delay);
+    });
+    try {
+      const result = await Promise.race([callback(...args), timer]);
+      clearTimeout(timerId);
+      return result;
+    } catch (err) {
+      clearTimeout(timerId);
+      throw err;
+    }
   };
