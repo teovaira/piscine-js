50e0d57b3ae50ba10f2c66e00d5dfd69b48df1cc
tvairakt <t.vairaktaris@gmail.com>
2026-05-28 10:28:58 +0300

    fix gougle-search: let timeout reject instead of returning Error

--- gougle-search.js
+++ gougle-search.js
@@ -2,24 +2,21 @@ const queryServers = (serverName, q) =>
   Promise.race([
     getJSON(`/${serverName}?q=${q}`),
     getJSON(`/${serverName}_backup?q=${q}`),
-  ])
+  ]);
 
 const gougleSearch = async (q) => {
   const timer = new Promise((_, reject) =>
-    setTimeout(() => reject(new Error('timeout')), 80)
-  )
+    setTimeout(() => reject(new Error("timeout")), 80),
+  );
 
-  try {
-    const [web, image, video] = await Promise.race([
-      Promise.all([
-        queryServers('web', q),
-        queryServers('image', q),
-        queryServers('video', q),
-      ]),
-      timer,
-    ])
-    return { web, image, video }
-  } catch (err) {
-    return new Error(err.message)
-  }
-}
+  const [web, image, video] = await Promise.race([
+    Promise.all([
+      queryServers("web", q),
+      queryServers("image", q),
+      queryServers("video", q),
+    ]),
+    timer,
+  ]);
+
+  return { web, image, video };
+};
