94f049c41c3fbe3347ae0d4878e52cebef7bb434
tvairakt <t.vairaktaris@gmail.com>
2026-05-29 09:45:22 +0300

    add race

---
+++ race.js
@@ -0,0 +1,16 @@
+const race = (promises) =>
+  new Promise((resolve, reject) => {
+    promises.forEach((p) => Promise.resolve(p).then(resolve).catch(reject));
+  });
+
+const some = (promises, count) =>
+  new Promise((resolve) => {
+    if (!promises.length || count === 0) return resolve(undefined);
+    const results = [];
+    promises.forEach((p) => {
+      Promise.resolve(p).then((val) => {
+        results.push(val);
+        if (results.length === count) resolve(results);
+      });
+    });
+  });
